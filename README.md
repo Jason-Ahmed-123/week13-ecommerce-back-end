bbe284d86f7356fd5c02b9ebe7f0e3638a8e0afb
Jason Ahmed <jason_ahmed@yahoo.com>
2020-08-12 11:41:19 -0700

    started updating route api files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# week13-ecommerce-back-end
-The back end for an e-commerce site, utilizing Express.js API configured to use Sequelize to interact with a MySQL database.
+# E-commerce Back End Starter Code
---
+++ routes/api/category-routes.js
@@ -0,0 +1,121 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+    // Access our User model and run .findAll() method)
+    Category.findAll({ 
+      attributes: [
+        'id',
+        'category_name'
+      ],
+       include: [
+      {
+          model: Product,
+          attributes: ['product_name']
+      }
+    ]
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({ 
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    where: {
+      id: req.params.id
+    },
+   include: [
+    {
+        model: Product,
+        attributes: ['product_name']
+        }
+      ]
+    }) 
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
+
+router.post('/', (req, res) => {
+  // create a new category
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  Category.create({
+    category_name: req.body.category_name
+    })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbCategoryData => {
+        if (!dbCategoryData) {
+          res.status(404).json({ message: 'No category found with this id' });
+          return;
+        }
+        res.json(dbCategoryData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
